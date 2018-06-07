e16f92943806796d72a30fd47dccdf217c986699
TImeth@ITelaSoft <timeth-itelasoft@Timeths-MacBook-Pro.local>
2018-06-07 11:09:31 +0530

    Added GET /todos and test scripts for it and created a start script in package.json

--- server/server.js
+++ server/server.js
@@ -25,6 +25,15 @@ app.post('/todos',(req,res)=>{
 
 })
 
+app.get('/todos',(req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    },(e) => {
+        res.status(400).send(e);
+    })
+})
+
+
 module.exports = {
     app
 }
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,18 +1,26 @@
 const expect = require('expect')
-const requst = require('supertest')
+const request = require('supertest')
 
 const{app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
+const todos = [{
+    text : "First test"
+},{
+    text : "Second test"
+}]
+
 beforeEach((done)=> {
-    Todo.remove({}).then(()=> done())
-})
+    Todo.remove({}).then(()=> {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+});
 
 describe('POST /todos', () => {
     it('Should create a new todo', (done) => {
         var text = 'Test todo text';
 
-        requst(app)
+        request(app)
         .post('/todos')
         .send({
             text
@@ -23,20 +31,20 @@ describe('POST /todos', () => {
         })
         .end((err,res) => {
             if (err){
-                return done(err)
+                return done(err);
             }
-            Todo.find().then((todos)=>{
-                expect(todos.length).toBe(1);
+            Todo.find({text}).then((todos)=>{
+                expect(todos.length).toBe;
                 expect(todos[0].text).toBe(text);
-                done()
-            }).catch(((e) => done(e)))
-        })
+                done();
+            }).catch(((e) => done(e)));
+        });
     });
 
 
     it('Should not create todo',(done) => {
         var text = 'Test todo text';
-        requst(app)
+        request(app)
         .post('/todos')
         .send({
             //text
@@ -47,9 +55,21 @@ describe('POST /todos', () => {
                 return done(err)
             }
             Todo.find().then((todos)=>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e)=> done(e))
-        })
-    })
+        });
+    });
 });
+
+describe ('GET /todos', () => {
+    it('Should get all todos',(done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    })
+})
