e7e23e1883d2a9079af76b74eb4294633fc2368c
timethsws <timethsws@gmail.com>
2018-06-07 17:17:16 +0530

    Created seperate env varibles for test dev and prod environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var url = process.env.AtlasURL || 'mongodb://localhost:27017/TodoApp'
+var url = process.env.AtlasURL;
 mongoose.connect(url);
 
 module.exports = {mongoose};
