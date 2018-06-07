5f1ec66e4d3c012a2610dab337ee18255dac747d
timethsws <timeth-itelasoft@Timeths-MacBook-Pro.local>
2018-06-07 15:06:15 +0530

    Added DELETE /todos/:id to routs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+//To user with heroku and mongodb atlas
 //mongoose.connect('mongodb+srv://admin_2:todo_app_admin@todoapp-jujgw.mongodb.net/test?retryWrites=true');
 
 mongoose.connect('mongodb://localhost:27017/TodoApp');
