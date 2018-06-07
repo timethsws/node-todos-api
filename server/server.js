1b4230277b87651dea21035eff63a2de7f31142f
TImeth@ITelaSoft <timeth-itelasoft@Timeths-MacBook-Pro.local>
2018-06-07 12:40:53 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
 
-var {mongoose} = require('./db/mongoose')
-var{Todo} = require('./models/todo')
-var{User} = require('./models/user')
+var { mongoose } = require('./db/mongoose')
+var { Todo } = require('./models/todo')
+var { User } = require('./models/user')
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-    
+app.post('/todos', (req, res) => {
+
     //console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     });
 
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc)
     }, (e) => {
         res.status(400).send(e)
@@ -25,12 +26,28 @@ app.post('/todos',(req,res)=>{
 
 })
 
-app.get('/todos',(req,res) => {
+app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos})
-    },(e) => {
+        res.send({ todos })
+    }, (e) => {
         res.status(400).send(e);
     })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id
+
+    Todo.findById(id)
+        .then((todo) => {
+            if(!todo){
+                return res.status(404).send({})
+            }
+            res.send({ todo });
+        }).catch((e) => {
+            res.status(400).send({});
+        })
+
+
 })
 
 
@@ -38,6 +55,6 @@ module.exports = {
     app
 }
 
-app.listen(3000, () =>{
-    console.log ('Started on port 3000')
+app.listen(3000, () => {
+    console.log('Started on port 3000')
 })
