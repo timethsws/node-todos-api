235bfdd4d45c9657dc2d0401ac426d623a862d6a
timethsws <timethsws@gmail.com>
2018-06-08 09:21:18 +0530

    Added auth for user and security

--- server/server.js
+++ server/server.js
@@ -104,6 +104,22 @@ app.patch('/todos/:id', (req,res) => {
 
 })
 
+
+app.post('/users',(req,res)=> {
+    var body = _.pick(req.body,['email','password'])
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth',token).send(user)
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+})
+
+
+
 module.exports = {
     app
 }
